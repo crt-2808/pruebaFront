39b2a8623bbefa619c624c8257e79bfa97f49321
Diego Heredia <0229536@up.edu.mx>
2024-01-13 20:14:36 -0600

    Baja Colab

--- src/components/editarColab.js
+++ src/components/editarColab.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./navbar";
-import { ArrowLeft } from "react-bootstrap-icons";
+import { ArrowLeft, Trash, Pencil } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import { showNotification } from "../utils/utils";
@@ -62,6 +62,40 @@ const EditarColab = () => {
   }, []);
   const handleImageError = (e) => {
     e.target.src = Usuario_sin_img; // imagen predeterminada
+  };
+  const handleDeleteClick = async (colaborador) => {
+    const razones = {
+      "baja voluntaria": "Baja voluntaria",
+      "baja por violar datos de prospecto":
+        "Baja por violar datos de prospecto",
+      "baja por falsificación de datos de prospecto":
+        "Baja por falsificación de datos de prospecto",
+      "baja por no reportar pagos a líder":
+        "Baja por no reportar pagos a líder",
+    };
+
+    const { value: razon } = await Swal.fire({
+      title: "Selecciona la razón de la baja",
+      input: "select",
+      inputOptions: razones,
+      inputPlaceholder: "Seleccionar tipo de baja",
+      showCancelButton: true,
+    });
+
+    if (razon) {
+      const confirmResult = await Swal.fire({
+        icon: "warning",
+        title: "Baja",
+        html: `<p>¿Quieres dar de baja a <strong>${colaborador.Nombre}</strong> por la razón: <strong>${razones[razon]}</strong>?</p>`,
+        showCancelButton: true,
+        confirmButtonText: "Dar baja",
+      });
+
+      if (confirmResult.isConfirmed) {
+        // Aquí va el código para dar de baja al colaborador
+        Swal.fire("Success", "Has dado de baja a un colaborador", "success");
+      }
+    }
   };
   const handleEditClick = async (colaborador) => {
     console.log("Colaborador identificado:  ", colaborador);
@@ -248,15 +282,18 @@ const EditarColab = () => {
           <h4>{`${data.Apellido_pat} ${data.Apellido_mat}`}</h4>
           <h6 className="email"> {data.Correo}</h6>
           <h6>{data.Telefono}</h6>
-          <svg
-            className="editar"
-            value={data.ID_Colab}
-            onClick={() => handleEditClick(data)}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
-          </svg>
+          <div className="col-12 centrar">
+            <Pencil
+              className="editar"
+              value={data.ID_Colab}
+              onClick={() => handleEditClick(data)}
+            />
+            <Trash
+              className="basura"
+              value={data.ID_Colab}
+              onClick={() => handleDeleteClick(data)}
+            />
+          </div>
         </div>
       </div>
     );
